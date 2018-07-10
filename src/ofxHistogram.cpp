183d6f3b004db7261b078aca2b16a207df34768e
BEAST5\Stephen Buchanan <stephen.buchanan@random.studio>
2018-07-10 15:15:50 +0200

    range corrected

--- src/ofxHistogram.cpp
+++ src/ofxHistogram.cpp
@@ -13,10 +13,10 @@ vector<float> ofxHistogram::getHistogram(ofxCvGrayscaleImage & img, int numBins)
     plane = &iplImage;
     
     int hist_size[] = { numBins };
-    float range[] = { 0, 180 };
+    float range[] = { 0, 256 };
     float* ranges[] = { range };
     hist = cvCreateHist( 1, hist_size, CV_HIST_ARRAY, ranges, 1 );
-    cvCalcHist( plane, hist, 0, 0 );
+    cvCalcHist( plane, hist );
     cvNormalizeHist( hist, 1.0 );
     
     for (int i=0; i<numBins; i++) {
